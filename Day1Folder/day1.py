10f077b024203dab2e9eb91512f55e2a982ff8a4
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-24 08:08:25 +0000

    Nov 24, 2025, 8:08 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,6 +1,7 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
 print("hello from day1")
+print("bacon")
 
 ########################################################################
 # Task 1:
@@ -9,33 +10,43 @@ print("hello from day1")
 
 ########################################################################
 # Task 2:
-
+ 
+print("My name is Elliot and im 13. I go to the school Mayflower secondary with my older sister. If i have 2000 dollars to spend, I would buy tickets to amusements parks.")
 
 
 ########################################################################
 # Task 3:
-
-
-
-########################################################################
+animal1 = "Tortise"
+animal2 = "salmon"
+animal3 = "bee"
+print(animal1)
+print(animal2)
+print(animal3)
 # Task 4:
-
-
-
+variable1 = 7
+variable2 = 5
+print(variable1+variable2)
+print(variable1*variable2)
+print(variable1/variable2)
 ########################################################################
 # Task 5:
-
-
-
+name = "Elliot"
+food = "pasta"
+print ("my name is " + name + " I like to eat " + food)
 ########################################################################
 # Task 6:
+def intro():
+    print ("My name is Elliot.")
+    print ("Im am 13 years old.")
+    print ("I got to the school called Mayflower secondary with my older sister.")
+    print ("If i have 2000 dollars to spend, I would buy tickets to the amusement park")
 
-
-
+intro()
 ########################################################################
 # Task 7:
+def addition( number1, number2):
+    print (number1 + number2)
 
-
-
+addition(243, 236)
 ########################################################################
 # Additional exercises:
