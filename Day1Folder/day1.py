8eb013240f34c7d98d5abaa7635e6d169f26993b
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-24 08:51:30 +0000

    Nov 24, 2025, 8:51 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -61,5 +61,15 @@ print("This is really simple but important")
 print("Python reads from the top to the bottom")
 print("Bye")
 
+##############################################################################
+# Exercise 2:
 
+# Store the your name in a variable called "name"
+# Print the contents of variable "name"
+
+# An example is given below
+
+# Write codes here
+greet = "hello"
+print(greet)
 
