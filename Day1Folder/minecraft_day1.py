3e877d28f69757550a69a21a18fcc8020a331751
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-24 08:32:02 +0000

    Nov 24, 2025, 8:32 AM

--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,46 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 ################# Function Section #############################
+def Teleport():
+    agent.teleport_to_player()
+# move commands
 
+def move_forward(steps):
+    agent.move(FORWARD, steps)
+
+def move_back(steps):
+    agent.move(BACK, steps)
+
+def move_up(steps):
+    agent.move(UP, steps)
+
+def move_down(steps):
+    agent.move(DOWN, steps)
+
+#turn commands
+def turn_left():
+    agent.turn(LEFT)
+
+def turn_right():
+    agent.turn(RIGHT)
+
+#obby1
+def obby1():
+    agent.move(FORWARD, 4)
+    agent.turn(LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(RIGHT)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 3)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
 
 ################## On Chat Commands Section #####################
