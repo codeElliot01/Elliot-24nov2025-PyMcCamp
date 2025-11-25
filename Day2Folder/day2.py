4864f4f68c8bc936fc54ce9e1268529037120e56
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-25 08:56:13 +0000

    Nov 25, 2025, 8:56 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -4,33 +4,59 @@ print("hello from day2")
 
 ########################################################################
 # Task 1:
-
+for count in range(100):
+    print("I will not sling mud at my friends")
 
 
 ########################################################################
 # Task 2:
-
+name = "ADAM"
+for letters in name:
+    print(letters)
 
 
 ########################################################################
 # Task 3:
+for counter in range(10):
+    print(counter)
 
 
 
 ########################################################################
 # Task 4:
+for count in range(1, 11):
+    print(count)
 
 
 
 ########################################################################
 # Task 5:
+for count in range(5, 33):
+    print (count)
 
 
 
 ########################################################################
 # Task 6:
-
+for count in range(50, 0, -1):
+    print (count)
 
 
 ########################################################################
 # Additional exercises:
+
+for count in range(67):
+    print (count)
+for count in range(101):
+    print (count)
+for count in range(7, 33):
+    print (count)
+for count in range(65, 100):
+    print (count)
+for count in range(2, 33, 2):
+    print (count)
+for count in range(5, 101, 5):
+    print (count)
+for count in range(100, -1, -1):
+    print (count)
+    
