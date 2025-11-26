2170a5521eaaba5e073ef63cac82d052c70c2031
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 07:04:42 +0000

    Nov 26, 2025, 7:04 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -32,7 +32,7 @@ price = int(input( " What is the price of the item?"))
 units = int(input( " how many are you buying?"))
 def multiplication ( price, units ):
     print (price * units)
-mut
+multiplication(price)
 
 
 ########################################################################
