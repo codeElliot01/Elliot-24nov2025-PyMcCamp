9ed0a1e9a6ce80ea643714c3e7953eadf5ea0c5c
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 06:31:41 +0000

    Nov 26, 2025, 6:31 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -12,6 +12,7 @@ print ( name + "is" + title + " so he ask his mother to" +command)
 ########################################################################
 # Task 2:
 name = Elliot
+num_pens
 
 
 
