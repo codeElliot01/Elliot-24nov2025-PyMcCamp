6ee9361641f2921e54f7d49c02b340ce8a5f0552
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 07:02:09 +0000

    Nov 26, 2025, 7:02 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -28,7 +28,7 @@ def addition ( num1, num2 ):
 addition(num1, num2)
 ########################################################################
 # Task 4:
-price = int 
+price = int(input( " "))
 units = int(input( " how many are you buying?"))
 
 
