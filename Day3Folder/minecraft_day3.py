3c57a8c28763915d576a950d85f5028b47c96fd0
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 09:09:02 +0000

    Nov 26, 2025, 9:09 AM

--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,12 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def build_wall(stone):
+    for i in range(stone):
+        agent.place(FORWARD)
+        agent.move(BACK, 1)
+
+def build_walls(stones):
+    for i in range(stones):
+        agent.place(FORWARD)
+        agent.move(UP, 1)
+        
