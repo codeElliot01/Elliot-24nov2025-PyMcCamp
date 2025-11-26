50f89269ab055b8eb3b10de7f607a46412f26090
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 08:05:09 +0000

    Nov 26, 2025, 8:05 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -68,7 +68,7 @@ num1 = random.randint(1, 100)
 num2 = random.randint(1, 100)
 ans = input( "What is the addition of " + str(num1) + "+" + str(num2) + "?")
 def addition ( num1, num2 ):
-    ans = (num1, num2)
+    correct_ans = (num1, num2)
 addition(num1, num2)
 if ans == ans:
     print( "You are smart!")
