07f9df719e1dc94e04b3635a700d4f8cba4eea70
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 07:29:02 +0000

    Nov 26, 2025, 7:29 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -40,8 +40,9 @@ print("hello from day3")
 age1 = int(input("Age1"))
 age2 = int(input("Age2"))
 if age1 > age2:
-    print(age1 + "is older than" age2)
-
+    print(age1 + "is older than" + age2)
+else:
+    
 ########################################################################
 # Task 6:
 
