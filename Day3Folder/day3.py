08b01b3a37db9f0f4cff5fc88255625aa45105bd
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 08:00:17 +0000

    Nov 26, 2025, 8:00 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -66,11 +66,8 @@ print("hello from day3")
 import random
 num1 = random.randint(1, 100)
 num2 = random.randint(1, 100)
-num1 = int(input ( what is the ))
-num2 = int(input ( ))
-def addition ( num1, num2 ):
-    print (num1 + num2)
-addition(num1, num2)
+ans = input( "What is the addition of " + num1 + "+" + num2)
+
 
 
 
