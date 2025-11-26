fc09e7d7f25dcc5281af6dbd820fd8e56eace1c6
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 09:09:17 +0000

    Nov 26, 2025, 9:09 AM

--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -9,4 +9,6 @@ def build_walls(stones):
     for i in range(stones):
         agent.place(FORWARD)
         agent.move(UP, 1)
-        
+
+player.on_chat("build_wall_down", build_wall)
+player.on_chat("build_wall_up", build_walls)
