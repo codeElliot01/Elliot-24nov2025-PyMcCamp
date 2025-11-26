01e244f9d09691a5518b9b958c4b5a13f29f1174
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-26 06:52:20 +0000

    Nov 26, 2025, 6:52 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -25,7 +25,7 @@ num1 = input ( "first number?")
 num2 = input ( "second number?")
 def addition ( num1, num2 ):
     print (num1 + num2)
-addition(num1)
+addition(num1, num2)
 ########################################################################
 # Task 4:
 
