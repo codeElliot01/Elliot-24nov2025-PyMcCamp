67a54b4f9ace86df9469288a5bbd901627ee9960
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-27 06:46:08 +0000

    Nov 27, 2025, 6:46 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -5,7 +5,7 @@ print("hello from day4")
 ########################################################################
 # Task 1:
 counter = 0
-while counter (counter < 10):
+while counter < 10:
     print (counter)
     counter += 1
 
