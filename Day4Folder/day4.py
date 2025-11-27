6b31199a293cc5d840ba0ddcbe3132e32e63fcd7
codeElliot01 <elliotkhoo2@gmail.com>
2025-11-27 06:51:08 +0000

    Nov 27, 2025, 6:51 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -17,7 +17,7 @@ while counter < 33:
 counter = 50
 while counter > 1:
     print (counter)
-    counter 
+    counter -+ 1
 ########################################################################
 # Task 2:
 
